ef0cc44796ecac1d8ed150f2e8540db285309071
przemek <przemyslaw.mazurek@netguru.pl>
2019-01-22 10:50:59 +0100

    django cms automatic version

--- setup_django.sh
+++ setup_django.sh
@@ -15,7 +15,6 @@ function check_environment_variables {
 
 function introduction {
 	echo "Welcome sir or madam...\nI'll be building your django today. How would you like it?"
-	echo "Lets start with the name of this project."
 }
 
 function set_mandatory_variables {
@@ -23,23 +22,15 @@ function set_mandatory_variables {
 	read -p "What name should I use? " project_name
 }
 
-function prompt_for_customizable_variables {
+function prompt_for_common_variables {
 	read -p "Which python version would you like me to use? " prompt
-		if [[ $prompt =~ ^[2-3]\.[0-7]$ ]]
-		then
-		    python_version=$prompt
-		else
-		    echo "Invalid Python version specified. It should contain only numbers and dots. e.g. 3.7"
-		    exit 1
-		fi
-	read -p "With which Django version would you like your project to use? " prompt
-		if [[ $prompt =~ ^[1-2]\.[0-9](\.[0-9])?$ ]]
-		then
-		    django_version=$prompt
-		else
-		    echo "Invalid Django version specified. It should contain only numbers and dots. e.g. 2.1.5"
-		    exit 1
-		fi
+	if [[ $prompt =~ ^[2-3]\.[0-7]$ ]]
+	then
+	    python_version=$prompt
+	else
+	    echo "Invalid Python version specified. It should contain only numbers and dots. e.g. 3.7"
+	    exit 1
+	fi
 	read -p "Do you want me to set up a Github repo as well? <Y/N> " prompt
 	if [[ $prompt =~ [yY](es)? ]]
 	then
@@ -58,16 +49,32 @@ function prompt_for_customizable_variables {
 
 function set_customizable_variables {
 	python_version=3.7
-	django_version=2.1.5
 	create_github=true
 	create_circleci=true
 }
 
-function set_required_variables {
-	set_mandatory_variables
+function set_required_django_variables {
 	if [ ! -z "$interactive" ]
 	then
-		prompt_for_customizable_variables
+		read -p "With which Django version would you like your project to use? " prompt
+		if [[ $prompt =~ ^[1-2]\.[0-9](\.[0-9])?$ ]]
+		then
+		    django_version=$prompt
+		else
+		    echo "Invalid Django version specified. It should contain only numbers and dots. e.g. 2.1.5"
+		    exit 1
+		fi
+		prompt_for_common_variables
+	else
+		django_version=2.1.5
+		set_customizable_variables
+	fi
+}
+
+function set_required_django_cms_variables {
+	if [ ! -z "$interactive" ]
+	then
+		prompt_for_common_variables
 	else
 		set_customizable_variables
 	fi
@@ -84,8 +91,6 @@ function create_django_file_structure {
 
 	cp scaffold/docker-compose.yml ${project_path}/docker-compose.yml
 
-	sed 's/<django_version>/Django=='"$django_version"'/g' scaffold/requirements.txt > ${project_path}/requirements.txt
-
 	mkdir ${project_path}/.circleci
 	sed 's/<python_version>/'"$python_version"'/g' scaffold/.circleci/config.yml | sed 's/<project_name>/'"$project_name"'/g' > ${project_path}/.circleci/config.yml
 }
@@ -99,6 +104,21 @@ function create_django_project {
 
 	rm -r ./${project_path}/${project_name}/update_settings
 }
+
+function create_django_cms_project {
+	cp scaffold/requirements.django_cms.txt ${project_path}/requirements.txt
+
+	docker-compose -f ${project_path}/docker-compose.yml run web djangocms -s -p . -r requirements.txt -m -u --no-db-driver ${project_name}
+
+	sed -i '' 's/djangocms-installer//g' ${project_path}/requirements.txt
+
+	cp -r update_settings ./${project_path}/${project_name}/
+
+	docker-compose -f ${project_path}/docker-compose.yml run web bash ${project_name}/update_settings/update_settings.sh
+
+	rm -r ${project_path}/${project_name}/update_settings
+}
+
 
 function call_github_api {
 	curl -i -H "Authorization: token ${GITHUB_SECRET}" -d "{\"name\": \"${project_name}\", \"auto_init\": false, \"private\": true}" https://api.github.com/user/repos
@@ -127,17 +147,63 @@ function start_containers {
 	docker-compose -f ${project_path}/docker-compose.yml up
 }
 
+function setup_django {
+	set_required_django_variables
+	create_django_file_structure
+
+	sed 's/<django_version>/Django=='"$django_version"'/g' scaffold/requirements.django.txt > ${project_path}/requirements.txt
+
+	create_django_project
+}
+
+function setup_django_cms {
+	set_required_django_cms_variables
+
+	create_django_file_structure
+
+	create_django_cms_project
+}
+
+function comming_up_soon {
+	echo "This option is not available yet, sorry."
+}
+
+function setup_project {
+read -p """What technology would you like me to use?
+1 - Django
+2 - Django-CMS
+3 - Django-REST-framework
+4 - Django-CMS and Django-REST-framework
+5 - Flask
+6 - Flask-RESTful
+7 - node.js
+""" technology_stack
+
+	case $technology_stack in
+		1) setup_django;;
+		2) setup_django_cms;;
+		3) comming_up_soon;;
+		4) comming_up_soon;;
+		5) comming_up_soon;;
+		6) comming_up_soon;;
+		7) comming_up_soon;;
+		[^1-7]*) echo "Invalid choice."
+	esac
+}
+
 function main {
 	[ ! -z "$verbosity" ] && set -x
 
 	check_environment_variables
-	introduction
-	set_required_variables
 
-	create_django_file_structure
-	create_django_project
+	introduction
+
+	set_mandatory_variables
+
+	setup_project
 
 	[ $create_github ] && setup_github_repo
+
 	[ $create_circleci ] && setup_circleci_project
 
 	start_containers
